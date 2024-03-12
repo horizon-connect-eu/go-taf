d22a16e7d1adce29b5cd98abcb443e37eecb45ba
Lea Baumstark <lea.baumstark@outlook.com>
2024-03-12 13:27:48 +0100

    add some comments

---
+++ pkg/message/doc.go
@@ -0,0 +1,14 @@
+// Description: This package contains the definitions of different message types used in the application.
+//
+// # message
+//
+// the [message.Message] type represents V2X messages that are received by the [gitlab-vs.informatik.uni-ulm.de/connect/taf-scalability-test/pkg/v2xlistener] and passed to the [tmm] and [tsm].
+//
+// # tasquery
+//
+// the [message.TasQuery] type represents request queries created in the [tas] and passed to the [tam].
+//
+// # tasresponse
+//
+// the [message.TasResponse] type represents response messages for a [tas] request, created in the [tam] and passed to the [tas].
+package message
