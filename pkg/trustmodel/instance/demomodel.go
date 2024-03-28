841864f22dd1da92a336659722ca225a325939c4
tcu38 <natasa.trkulja@uni-ulm.de>
2024-03-28 16:03:32 +0100

    updates to the trust model instance

--- pkg/trustmodel/instance/demomodel.go
+++ pkg/trustmodel/instance/demomodel.go
@@ -1,31 +1,44 @@
 package instance
 
+import (
+	"github.com/vs-uulm/taf-tlee-interface/pkg/subjectivelogic"
+	"github.com/vs-uulm/taf-tlee-interface/pkg/trustmodelstructure"
+)
+
 type TrustModelInstance struct {
 	id          int
 	tmt         string
-	omega1      float64
-	omega2      float64
+	omega1      subjectivelogic.Opinion
+	omega2      subjectivelogic.Opinion
 	version     int
 	fingerprint int
+	omega_DTI_1 subjectivelogic.Opinion
+	omega_DTI_2 subjectivelogic.Opinion
+	weights     [3]float64
 }
 
 func NewTrustModelInstance(id int, tmt string) TrustModelInstance {
 	return TrustModelInstance{
-		id:     id,
-		tmt:    tmt,
-		omega1: 0,
-		omega2: 0,
+		id:          id,
+		tmt:         tmt,
+		omega_DTI_1: subjectivelogic.Opinion{Belief: 0.2, Disbelief: 0.1, Uncertainty: 0.7, BaseRate: 0.5},
+		omega_DTI_2: subjectivelogic.Opinion{Belief: 0.15, Disbelief: 0.15, Uncertainty: 0.7, BaseRate: 0.5},
+		weights:     [3]float64{0.2, 0.4, 0.4},
+		omega1:      subjectivelogic.Opinion{Belief: 0.2, Disbelief: 0.1, Uncertainty: 0.7, BaseRate: 0.5},
+		omega2:      subjectivelogic.Opinion{Belief: 0.15, Disbelief: 0.15, Uncertainty: 0.7, BaseRate: 0.5},
+		version:     0,
+		fingerprint: -1,
 	}
 }
 
 // TODO: Implement return hardcoded structure of this trust model instance
-func (i *TrustModelInstance) getStructure() {
-
+func (i *TrustModelInstance) getStructure() (trustmodelstructure.Structure) {
+	return nil
 }
 
-// TODO: Implement return values of this trust model instance
-func (i *TrustModelInstance) getValues() {
-
+// TODO: Implement return of all Trust Opinions (values) of this trust model instance
+func (i *TrustModelInstance) getValues() (map[string]subjectivelogic.Opinion) {
+	return nil
 }
 
 func (i *TrustModelInstance) GetId() int {
