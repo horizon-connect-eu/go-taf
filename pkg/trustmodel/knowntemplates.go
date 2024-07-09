6d63de319c3ce507b6dcc240a09beaabc0444d32
Benjamin Erb <benjamin.erb@uni-ulm.de>
2024-07-09 08:45:02 +0200

    Skeleton for CACC TMT

--- pkg/trustmodel/knowntemplates.go
+++ pkg/trustmodel/knowntemplates.go
@@ -1,6 +1,7 @@
 package trustmodel
 
 import (
+	"github.com/vs-uulm/go-taf/pkg/trustmodel/models/cooperativeadaptivecruisecontrol"
 	"github.com/vs-uulm/go-taf/pkg/trustmodel/models/examplemodel"
 	"github.com/vs-uulm/go-taf/pkg/trustmodel/models/intersectionmovementassist"
 	"github.com/vs-uulm/go-taf/pkg/trustmodel/trustmodeltemplate"
@@ -9,4 +10,5 @@ import (
 var KnownTemplates = map[string]trustmodeltemplate.TrustModelTemplate{
 	"EXAMPLE@0.0.1": examplemodel.CreateTrustModelTemplate("EXAMPLE", "0.0.1"),
 	"IMA@0.0.1":     intersectionmovementassist.CreateTrustModelTemplate("IMA", "0.0.1"),
+	"CACC@0.0.1":    cooperativeadaptivecruisecontrol.CreateTrustModelTemplate("CACC", "0.0.1"),
 }
---
+++ pkg/trustmodel/models/cooperativeadaptivecruisecontrol/cacc.go
@@ -0,0 +1,77 @@
+package cooperativeadaptivecruisecontrol
+
+import (
+	"fmt"
+	"github.com/vs-uulm/go-taf/pkg/trustmodel/trustmodelinstance"
+	"github.com/vs-uulm/go-taf/pkg/trustmodel/trustmodeltemplate"
+	"github.com/vs-uulm/taf-tlee-interface/pkg/trustmodelstructure"
+	"math/rand/v2"
+)
+
+type TrustModelTemplate struct {
+	name    string
+	version string
+}
+
+func CreateTrustModelTemplate(name string, version string) trustmodeltemplate.TrustModelTemplate {
+	return TrustModelTemplate{
+		name:    name,
+		version: version,
+	}
+}
+
+func (t TrustModelTemplate) Version() string {
+	return t.version
+}
+
+func (t TrustModelTemplate) TemplateName() string {
+	return t.name
+}
+
+func (t TrustModelTemplate) Spawn(params map[string]string) trustmodelinstance.TrustModelInstance {
+	return &TrustModelInstance{
+		id:       t.TemplateName() + "@" + t.Version() + "-" + fmt.Sprintf("%000000d", rand.IntN(999999)),
+		version:  0,
+		template: t,
+	}
+}
+
+type TrustModelInstance struct {
+	id      string
+	version int
+
+	template TrustModelTemplate
+}
+
+func (e *TrustModelInstance) ID() string {
+	return e.id
+}
+
+func (e *TrustModelInstance) Version() int {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (e *TrustModelInstance) Fingerprint() uint32 {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (e *TrustModelInstance) Structure() trustmodelstructure.TrustGraphStructure {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (e *TrustModelInstance) Values() map[string][]trustmodelstructure.TrustRelationship {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (e *TrustModelInstance) Template() string {
+	return e.template.TemplateName() + "@" + e.template.Version()
+}
+
+func (e *TrustModelInstance) Update() {
+	//TODO implement me
+	panic("implement me")
+}
