79d80d4e63095fb82385380e37f406bdb7eeb4ce
Artur Hermann <Artur.Hermann@uni-ulm.de>
2024-07-10 14:12:54 +0200

    Config specification added to playback component

--- cmd/playback/playback.go
+++ cmd/playback/playback.go
@@ -11,7 +11,6 @@ import (
 	"github.com/vs-uulm/go-taf/pkg/config"
 	"github.com/vs-uulm/go-taf/pkg/core"
 	"github.com/vs-uulm/go-taf/plugins/communication/kafkabased"
-	"log"
 	"log/slog"
 	"os"
 	"path/filepath"
@@ -24,14 +23,37 @@ import (
 A helper command to play back test workloads via Kafka.
 */
 func main() {
+	//specification of testcase -> directory name in workloads folder
+	testcase := flag.String("story", "", "path to the directory with the storyline specification - should include a script.csv file and the single json messages")
+
+	configPath := flag.String("config", "", "path to the file with the configuration specification")
+
+	//specification of target
+	target := flag.Bool("target", false, "list of targets can be specified - if targets are provided, messages are send from the playback tool only to these targets (all other messages are filtered out)")
+	var targetEntities []string
+
+	flag.Parse()
+
 	//crypto.Init()
 	tafConfig := config.DefaultConfig
-	// First, see whether a config file path has been specified
-	if filepath, ok := os.LookupEnv("TAF_CONFIG"); ok {
+
+	if *configPath != "" {
+		var err error
+		tafConfig, err = config.LoadJSON(*configPath)
+
+		if err != nil {
+			//log.Fatalf("main: error reading config file %s: %s\n", configFile, err.Error())
+			fmt.Fprintln(os.Stderr, "Config parameter is incorrect - specified file "+*configPath+" not found")
+			fmt.Fprintln(os.Stderr, "usage:   ./playback -story=path [-config=path] [-target target list]")
+			fmt.Fprintln(os.Stderr, "example: ./playback -story=storydirectory/storyline1 -config=configdirectory/config1.json -target taf aiv mbd")
+			os.Exit(1)
+		}
+	} else if filepath, ok := os.LookupEnv("TAF_CONFIG"); ok {
 		var err error
 		tafConfig, err = config.LoadJSON(filepath)
 		if err != nil {
-			log.Fatalf("main: error reading config file %s: %s\n", filepath, err.Error())
+			//log.Fatalf("main: error reading config file %s: %s\n", filepath, err.Error())
+			fmt.Fprintln(os.Stderr, "Environment variable is incorrect - specified file "+filepath+" not found")
 		}
 	}
 
@@ -67,15 +89,6 @@ func main() {
 
 	defer time.Sleep(1 * time.Second) // TODO: replace this cleanup interval with waitgroups
 	defer cancelFunc()
-
-	//specification of testcase -> directory name in workloads folder
-	testcase := flag.String("story", "", "path to the directory with the storyline specification - should include a script.csv file and the single json messages") //default testcase is example
-
-	//specification of target
-	target := flag.Bool("target", false, "list of targets can be specified - if targets are provided, messages are send from the playback tool only to these targets (all other messages are filtered out)")
-	var targetEntities []string
-
-	flag.Parse()
 
 	if *testcase == "" {
 		fmt.Fprintln(os.Stderr, "Story parameter is missing - please use the story parameter to specify the directory of the story line")
