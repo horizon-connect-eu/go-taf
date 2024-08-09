194ca238e59a2afd674ef2a32146af235b71bc3f
Benjamin Erb <benjamin.erb@uni-ulm.de>
2024-08-09 12:21:29 +0200

    add description to BRUSSELS@0.0.1

--- plugins/trustmodels/brussels/brussels.go
+++ plugins/trustmodels/brussels/brussels.go
@@ -3,5 +3,5 @@ package brussels
 import "github.com/vs-uulm/go-taf/pkg/trustmodel"
 
 func init() {
-	trustmodel.RegisterTemplate(CreateTrustModelTemplate("BRUSSELS", "0.0.1", "TODO: description"))
+	trustmodel.RegisterTemplate(CreateTrustModelTemplate("BRUSSELS", "0.0.1", "The BRUSSELS Trust Model is a demo model used to evaluate the trustworthiness of two vehicle computers on an ego vehicle."))
 }
