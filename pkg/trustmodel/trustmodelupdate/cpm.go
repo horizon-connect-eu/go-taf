78a46a0922c45b87a5e8c3aa2d44dbcf6d1c6087
Benjamin Erb <benjamin.erb@uni-ulm.de>
2024-09-20 15:05:52 +0200

    add RefreshCPM update

---
+++ pkg/trustmodel/trustmodelupdate/cpm.go
@@ -0,0 +1,26 @@
+package trustmodelupdate
+
+import "github.com/vs-uulm/go-taf/pkg/core"
+
+type RefreshCPM struct {
+	sourceID string
+	objects  []string
+}
+
+func (r RefreshCPM) SourceID() string {
+	return r.sourceID
+}
+func (r RefreshCPM) Objects() []string {
+	return r.objects
+}
+
+func CreateRefreshCPM(sourceID string, objects []string) RefreshCPM {
+	return RefreshCPM{
+		sourceID: sourceID,
+		objects:  objects,
+	}
+}
+
+func (u RefreshCPM) Type() core.UpdateOp {
+	return core.REFRESH_CPM
+}
