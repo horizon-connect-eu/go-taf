15b0cbfc0e84d3b382d8f005c62972c01e4c638a
Benjamin Erb <benjamin.erb@uni-ulm.de>
2024-09-23 11:47:15 +0200

    implementation of IMA TMI to handle dynamic topologies

--- plugins/trustmodels/intersectionmovementassist/tmi.go
+++ plugins/trustmodels/intersectionmovementassist/tmi.go
@@ -5,9 +5,13 @@ import (
 	"github.com/vs-uulm/go-subjectivelogic/pkg/subjectivelogic"
 	"github.com/vs-uulm/go-taf/internal/util"
 	"github.com/vs-uulm/go-taf/pkg/core"
+	internaltrustmodelstructure "github.com/vs-uulm/go-taf/pkg/trustmodel/trustmodelstructure"
 	"github.com/vs-uulm/go-taf/pkg/trustmodel/trustmodelupdate"
 	"github.com/vs-uulm/taf-tlee-interface/pkg/trustmodelstructure"
+	"hash/fnv"
 	"log/slog"
+	"sort"
+	"strings"
 )
 
 type TrustModelInstance struct {
@@ -16,6 +20,11 @@ type TrustModelInstance struct {
 	template TrustModelTemplate
 
 	sourceID string
+	objects  map[string]bool
+
+	currentStructure   trustmodelstructure.TrustGraphStructure
+	currentValues      map[string][]trustmodelstructure.TrustRelationship
+	currentFingerprint uint32
 }
 
 func (e *TrustModelInstance) ID() string {
@@ -23,24 +32,11 @@ func (e *TrustModelInstance) ID() string {
 }
 
 func (e *TrustModelInstance) Version() int {
-	return 0
+	return e.version
 }
 
 func (e *TrustModelInstance) Fingerprint() uint32 {
-	//TODO implement me
-	//panic("implement me")
-	return 0
-}
-
-func (e *TrustModelInstance) Structure() trustmodelstructure.TrustGraphStructure {
-	//TODO implement me
-	//panic("implement me")
-	return nil
-}
-
-func (e *TrustModelInstance) Values() map[string][]trustmodelstructure.TrustRelationship {
-	//TODO implement me
-	return nil
+	return e.currentFingerprint
 }
 
 func (e *TrustModelInstance) Template() core.TrustModelTemplate {
@@ -48,34 +44,166 @@ func (e *TrustModelInstance) Template() core.TrustModelTemplate {
 }
 
 func (e *TrustModelInstance) Update(update core.Update) bool {
-	//TODO implement me
+	oldVersion := e.Version()
 	switch update := update.(type) {
 	case trustmodelupdate.RefreshCPM:
-		//TODO: implement
-		slog.Warn("Received Update from Source: " + update.SourceID())
-		slog.Warn("Contained IDs: " + fmt.Sprintf("%+v", update.Objects()))
+		topologyIsModified := e.processTopologyUpdate(update.Objects())
+		if topologyIsModified {
+			e.updateStructure()
+			e.updateFingerprint()
+			e.incrementVersion()
+			slog.Warn(fmt.Sprintf("%s", e))
+		}
 	case trustmodelupdate.UpdateAtomicTrustOpinion:
 		//TODO
 		util.UNUSED(update)
 	default:
 		//ignore
 	}
-	return true
+	return oldVersion != e.Version() //when version has changed, indicate to run TLEE
 }
 
-func (e *TrustModelInstance) TrustSourceQuantifiers() []core.TrustSourceQuantifier {
-	return []core.TrustSourceQuantifier{}
+func (e *TrustModelInstance) incrementVersion() int {
+	e.version = e.version + 1
+	return e.version
+}
+
+/*
+processTopologyUpdate reflects changes in the internal topology based upon the latest objects received in an update.
+In case there are topology changes due to the update, the function returns true. Otherwise, if the topology is not affected, it returns false.
+*/
+func (e *TrustModelInstance) processTopologyUpdate(latestObjects []string) bool {
+	topologyChanged := false
+
+	removedObjects := make(map[string]struct{}) //old objects will be placed here and will be deleted in case they are still used
+	addedObjects := make(map[string]struct{})
+	for k, _ := range e.objects {
+		removedObjects[k] = struct{}{}
+	}
+
+	for _, object := range latestObjects {
+		if _, ok := e.objects[object]; ok {
+			//object existed before, so remove from the set of missing objects
+			delete(removedObjects, object)
+		} else {
+			//object is not yet known, so needs to be added
+			addedObjects[object] = struct{}{}
+		}
+	}
+	if len(addedObjects) > 0 {
+		topologyChanged = true
+		for object, _ := range addedObjects {
+			e.objects[object] = true
+		}
+
+	}
+	if len(removedObjects) > 0 {
+		topologyChanged = true
+		for object, _ := range removedObjects {
+			delete(e.objects, object)
+		}
+	}
+	return topologyChanged
+}
+
+/*
+updateFingerprint calculates the current fingerprint for the TMI.
+Therefore, it takes the all the dynamic nodes, concatenates their
+sorted string identifiers and calculates a hash value.
+*/
+func (e *TrustModelInstance) updateFingerprint() {
+	nodes := make([]string, len(e.objects))
+	for object := range e.objects {
+		nodes = append(nodes, object)
+	}
+
+	sort.Strings(nodes)
+	stringFingerprint := strings.Join(nodes, "")
+
+	algorithm := fnv.New32a()
+	algorithm.Write([]byte(stringFingerprint))
+	e.currentFingerprint = algorithm.Sum32()
+}
+
+/*
+updateStructure updates the internally kept structure according to the latest topology.
+*/
+func (e *TrustModelInstance) updateStructure() {
+	//Objects (observations) that originate from the sender vehicle
+	objects := make([]string, len(e.objects))
+	for object := range e.objects {
+		objects = append(objects, objectIdentifier(object, e.sourceID))
+	}
+
+	//The root node (ego) vehicles has edges to all observations and the sender vehicle
+	egoTargets := make([]string, len(e.objects)+1)
+	copy(egoTargets, objects)
+	egoTargets = append(egoTargets, vehicleIdentifier(e.sourceID))
+
+	e.currentStructure = internaltrustmodelstructure.NewTrustGraphDTO("CumulativeFusion", []trustmodelstructure.AdjacencyListEntry{
+		internaltrustmodelstructure.NewAdjacencyEntryDTO(vehicleIdentifier("ego"), egoTargets),
+		internaltrustmodelstructure.NewAdjacencyEntryDTO(vehicleIdentifier(e.sourceID), objects),
+	})
+	//slog.Warn(internaltrustmodelstructure.TGSAsString(e.currentStructure))
+}
+
+/*
+updateValues updates the internally kept values according to the latest state.
+*/
+func (e *TrustModelInstance) updateValues() {
+	//TODO
 }
 
 func (e *TrustModelInstance) Initialize(params map[string]interface{}) {
-	//TODO: get source ID from params
+	//If a source ID has been defined, use it; otherwise, use ID of TMI
+	sourceId, exists := params["SourceId"]
+	if !exists {
+		e.sourceID = e.id
+	} else {
+		e.sourceID = sourceId.(string)
+	}
+	e.version = 0
+	e.currentFingerprint = 0
 	return
 }
 
 func (e *TrustModelInstance) Cleanup() {
+	//nothing to do
 	return
 }
 
+func (e *TrustModelInstance) Structure() trustmodelstructure.TrustGraphStructure {
+	return e.currentStructure
+}
+
+func (e *TrustModelInstance) Values() map[string][]trustmodelstructure.TrustRelationship {
+	return e.currentValues
+}
+
 func (e *TrustModelInstance) RTLs() map[string]subjectivelogic.QueryableOpinion {
+	//TODO: implement
 	return map[string]subjectivelogic.QueryableOpinion{}
+}
+
+func (e *TrustModelInstance) TrustSourceQuantifiers() []core.TrustSourceQuantifier {
+	//TODO: implement
+	return []core.TrustSourceQuantifier{}
+}
+
+/*
+vehicleIdentifier is a helper function to turn a plain identifier into an identifier for vehicles used in the structure.
+*/
+func vehicleIdentifier(id string) string {
+	return fmt.Sprintf("V_%s", id)
+}
+
+/*
+objectIdentifier is a helper function to turn a plain identifier into an identifier for objects/observations used in the structure.
+*/
+func objectIdentifier(id string, source string) string {
+	return fmt.Sprintf("C_%s_%s", source, id)
+}
+
+func (e *TrustModelInstance) String() string {
+	return core.DumpTMI(e)
 }
