72f1c1b81c31c578f55d5b225d1b6c966e84ae74
Benjamin Erb <benjamin.erb@uni-ulm.de>
2024-09-23 20:53:00 +0200

    internal TLEE with logger

--- pkg/tlee/tlee.go
+++ pkg/tlee/tlee.go
@@ -2,13 +2,18 @@ package tlee
 
 import (
 	"github.com/vs-uulm/go-subjectivelogic/pkg/subjectivelogic"
+	trustmodelstructure2 "github.com/vs-uulm/go-taf/pkg/trustmodel/trustmodelstructure"
 	"github.com/vs-uulm/taf-tlee-interface/pkg/trustmodelstructure"
+	"log/slog"
 )
 
 type TLEE struct {
+	Logger *slog.Logger
 }
 
 func (t *TLEE) RunTLEE(trustmodelID string, version int, fingerprint uint32, structure trustmodelstructure.TrustGraphStructure, values map[string][]trustmodelstructure.TrustRelationship) map[string]subjectivelogic.QueryableOpinion {
+
+	t.Logger.Info("TLEE Input", "Graph Structure", trustmodelstructure2.DumpStructure(structure))
 	results := make(map[string]subjectivelogic.QueryableOpinion)
 
 	for _, list := range values {
