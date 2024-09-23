285030a07f84b9412151cf648a4eae1519d8b5d3
Benjamin Erb <benjamin.erb@uni-ulm.de>
2024-09-23 20:52:02 +0200

    add logger to internal TLEE

--- cmd/main.go
+++ cmd/main.go
@@ -80,7 +80,9 @@ func main() {
 
 	var tlee tleeinterface.TLEE
 	if tafConfig.TLEE.UseInternalTLEE {
-		tlee = &internaltlee.TLEE{}
+		tlee = &internaltlee.TLEE{
+			Logger: logging.CreateChildLogger(tafContext.Logger, "Internal TLEE"),
+		}
 	} else {
 		tlee = &actualtlee.TLEE{}
 	}
