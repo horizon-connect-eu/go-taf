fbd8901451888ee8b059395a92a869414572963c
Benjamin Erb <benjamin.erb@uni-ulm.de>
2024-10-21 09:17:47 +0200

    don't use separate go-routine for sending event to channel

--- pkg/web/taflistener.go
+++ pkg/web/taflistener.go
@@ -5,45 +5,29 @@ import (
 )
 
 func (s *Webserver) OnSessionCreated(event listener.SessionCreatedEvent) {
-	go func() {
-		s.listenerChannel <- event
-	}()
+	s.listenerChannel <- event
 }
 
 func (s *Webserver) OnSessionTorndown(event listener.SessionDeletedEvent) {
-	go func() {
-		s.listenerChannel <- event
-	}()
+	s.listenerChannel <- event
 }
 
 func (s *Webserver) OnATLUpdated(event listener.ATLUpdatedEvent) {
-	go func() {
-		s.listenerChannel <- event
-	}()
-
+	s.listenerChannel <- event
 }
 
 func (s *Webserver) OnATLRemoved(event listener.ATLRemovedEvent) {
-	go func() {
-		s.listenerChannel <- event
-	}()
+	s.listenerChannel <- event
 }
 
 func (s *Webserver) OnTrustModelInstanceSpawned(event listener.TrustModelInstanceSpawnedEvent) {
-	go func() {
-		s.listenerChannel <- event
-	}()
+	s.listenerChannel <- event
 }
 
 func (s *Webserver) OnTrustModelInstanceUpdated(event listener.TrustModelInstanceUpdatedEvent) {
-	go func() {
-		s.listenerChannel <- event
-	}()
+	s.listenerChannel <- event
 }
 
 func (s *Webserver) OnTrustModelInstanceDeleted(event listener.TrustModelInstanceDeletedEvent) {
-	go func() {
-		s.listenerChannel <- event
-	}()
-
+	s.listenerChannel <- event
 }
